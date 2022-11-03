ce305f3673b42b9afd4901c5c29a73895d442363
thanvi47 <77586167+thanvi47@users.noreply.github.com>
2022-11-03 23:22:17 +0600

    add templates

---
+++ resources/views/admin/layouts/create.blade.php
@@ -0,0 +1,24 @@
+@include('admin.layouts.navbar')
+
+@include('admin.layouts.sidebar')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('You are logged in!') }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @yield('content')
+    @include('admin.layouts.footer')
---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,6 @@
+@include('admin.layouts.navbar')
+
+@include('admin.layouts.sidebar')
+{{--@include('admin.layouts.content')--}}
+@yield('content')
+@include('admin.layouts.footer')
